d8c0a96809188d70c184ab9740e163cf9ea6f71a
Reincor12 <temsanych2019@gmail.com>
2025-07-23 21:40:48 +0300

    Delete animation

--- js.js
+++
@@ -1,9 +0,0 @@
-
-let searchBttn = document.querySelector('#search-bttn')
-let search = document.querySelector('#search')
-searchBttn.addEventListener('mouseover', function(){
-    search.style.opacity = '1'
-})
-searchBttn.addEventListener('mouseout', function(){
-    search.style.opacity = '0'
-})
